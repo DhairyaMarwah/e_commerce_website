fe7f00cd5af6f903d4356d6d2ae8cda479debf53
dhairyamarwah <dhairyamarwah01@gmail.com>
2022-07-21 20:10:56 +0530

    update

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce('pk_38867a5453a485ad3586895c7841f1eab8d84e4d20e2b', true);
